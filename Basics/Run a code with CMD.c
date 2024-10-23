421f188720db08c2d6228baa96b3788f4c23b437
Gurey Jama <gureyjama743@gmail.com>
2024-10-23 12:39:17 +0300

    initial commit

---
+++ Basics/Run a code with CMD.c
@@ -0,0 +1,4 @@
+1. Make sure you have a gcc compiler
+2. Change active working directory to the folder contain C file
+3. Compile the C file with gcc and name of the file
+4. Run the compiled file. 
---
+++ Basics/Variables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  //Here we shall learn variables. 
+  //What is a variable? 
+  //A variable is just a way to give a name to a place in your computer's memory where you store data.
+  //For xample :
+  int age=0; // 'age' is the name of the variable
+  printf("The age of child is : %d",age);
+
+
+}
