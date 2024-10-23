7df54fea25d6e5386bf5b968ae75720a23851dee
Gurey Jama <gureyjama743@gmail.com>
2024-10-23 15:50:30 +0300

    Updated the escape sequence file

---
+++ Basics/Escapse_Sequences.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+   //Escape sequences
+   //What is a scape sequence?
+   /*A escape sequence is a character combination consisting of a backslash \ followed by a letter or 
+   cobination of digits .
+   They specifiy actions within a line or string of a text.
+   \n= newline
+   \t= tab*/
+
+//Example:
+printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+
+//To add quotes.
+printf("\"I sleep less\" - Our teacher\n");
+printf("\'I have to work hard so that i make my parents happy\' Najib Jama\n");
+printf("Sanadka 2025 Alle ha nooga dhigo mid barwaaqo iyo guulo badan i.a.\n");
+printf("\"Nin lagu seexdoow ha seexan!\" -My mother\n");
+printf("2025\t10\t23\t3:27PM\n");
+printf("Happy coding!");
+
+
+
+
+
+
+}
