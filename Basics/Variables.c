ca3a3dc390004d09db200e984417b0147326d8e2
Gurey Jama <gureyjama743@gmail.com>
2024-10-30 11:53:26 +0300

    initial commit

--- Basics/Variables.c
+++ Basics/Variables.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 int main()
 {
-  //Here we shall learn variables. 
-  //What is a variable? 
+  //Here we shall learn variables.
+  //What is a variable?
   //A variable is just a way to give a name to a place in your computer's memory where you store data.
   //For xample :
   int age=0; // 'age' is the name of the variable
   printf("The age of child is : %d\n",age);
-  
+
   //creating a variable is done in two steps:
     /* 1.declaration
        2.initialization
-       
+
     */
 
    int x; //declartion
    x=50; //initialization
    char grade= 'A';
    char name[]=  "Najib Jama"; //an array of characters.
-    
-  printf("Hello %s\n",name);  
+
+  printf("Hello %s\n",name);
   printf("Your average grade is : %c",grade);
-  
+
 
 
 
